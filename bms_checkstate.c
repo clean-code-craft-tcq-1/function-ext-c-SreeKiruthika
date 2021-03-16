12df94ee89d8c2bbddd2de7655d5c8f86da261d9
SreeKiruthika <77397602+SreeKiruthika@users.noreply.github.com>
2021-03-17 00:22:54 +0530

    Add files via upload

--- bms_checkstate.c
+++ bms_checkstate.c
@@ -24,7 +24,7 @@ int BatteryParamCheck(float param_value, float min_value, float max_value, enum 
 		retval = IN_RANGE;
 	}
 	
-	paramStatus[param_name] =  retval;
+	int paramStatus[param_name] =  retval;
 	return retval;
 }	
 
